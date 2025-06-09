2a40615ea017cb7fe719b8fc7d3500e29401ffbd
gpostVT <gep100iosgames@gmail.com>
2025-06-08 22:10:46 -0400

    fix: move posts and projects into src folder

--- src/components/MDXContent.tsx
+++ src/components/MDXContent.tsx
@@ -40,33 +40,39 @@ export default function MDXContent({
           const projects = [
             {
               name: "The Odin Project",
-              location: "/public/projects/the-odin-project.mdx",
+              location: "/src/content/projects/the-odin-project.mdx",
             },
-            { name: "Clariphoto", location: "/public/projects/clariphoto.mdx" },
+            {
+              name: "Clariphoto",
+              location: "/src/content/projects/clariphoto.mdx",
+            },
             {
               name: "Playlist Transfers",
-              location: "/public/projects/playlist-transfers.mdx",
+              location: "/src/content/projects/playlist-transfers.mdx",
             },
-            { name: "Reformify", location: "/public/projects/reformify.mdx" },
+            {
+              name: "Reformify",
+              location: "/src/content/projects/reformify.mdx",
+            },
             {
               name: "National Parks",
-              location: "/public/projects/national-parks.mdx",
+              location: "/src/content/projects/national-parks.mdx",
             },
             {
               name: "Robin (Unfinished)",
-              location: "/public/projects/robin.mdx",
+              location: "/src/content/projects/robin.mdx",
             },
             {
               name: "Better Job Board (Unfinished)",
-              location: "/public/projects/better-job-board.mdx",
+              location: "/src/content/projects/better-job-board.mdx",
             },
             {
               name: "Sports Prediction ML Models",
-              location: "/public/projects/sports-prediction-ml-models.mdx",
+              location: "/src/content/projects/sports-prediction-ml-models.mdx",
             },
             {
               name: "Spontaneous",
-              location: "/public/projects/spontaneous.mdx",
+              location: "/src/content/projects/spontaneous.mdx",
             },
           ];
 
@@ -85,9 +91,9 @@ export default function MDXContent({
           // Extract the filename from the location
           const filename =
             project.location.split("/").pop()?.replace(".mdx", "") || "";
-          importPath = `../../public/projects/${filename}.mdx`;
+          importPath = `../../src/content/projects/${filename}.mdx`;
         } else {
-          importPath = `../../public/posts/${postId}.mdx`;
+          importPath = `../../src/content/posts/${postId}.mdx`;
         }
 
         // Dynamically import the MDX file
